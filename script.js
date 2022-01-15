e1baf7d9a40d62213c8cae1c71086b7797718099
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-01-15 14:11:27 -0300

    Ação botão ajustada

--- script.js
+++ script.js
@@ -1,33 +1,86 @@
-const imagem = document.querySelector('img');
 const botao = document.querySelector('#botao');
-const nomeDoPersonagem = document.querySelector('#nome1');
-const especie = document.querySelector('#especie1');
-const condicao = document.querySelector('#status1');
+
+const imagem1 = document.querySelector('#personagem1');
+const nomeDoPersonagem1 = document.querySelector('#nome1');
+const especie1 = document.querySelector('#especie1');
+const condicao1 = document.querySelector('#status1');
+
+const imagem2 = document.querySelector('#personagem2');
+const nomeDoPersonagem2 = document.querySelector('#nome2');
+const especie2 = document.querySelector('#especie2');
+const condicao2 = document.querySelector('#status2');
+
+const imagem3 = document.querySelector('#personagem3');
+const nomeDoPersonagem3 = document.querySelector('#nome3');
+const especie3 = document.querySelector('#especie3');
+const condicao3 = document.querySelector('#status3');
 
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
 
-pegarPersonagem = () => {
-    let numeroAleatorio = gerarValorAleatorio()
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+pegarPersonagem1 = () => {
+    let numeroAleatorioPersonagem1 = gerarValorAleatorio()
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorioPersonagem1}`, {
         method:'GET',
         headers: {
             Accept: 'application/json',
             "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
-        imagem.src = data.image;
-        imagem.alt=data.name;
-        nomeDoPersonagem.innerHTML = data.name;
-        especie.innerHTML = data.species;
-        condicao.innerHTML= data.status;
-        // quando clica, gera um numero aleatório que muda o nome la no html referenciando o personagem
-    }); 
-        // fetch é uma função que faz uma chamada, um endereço tipo "fetch liga p API do rick e faça algo"
-        // `` dois acentos é onde coloca o endereço da API rick
+        imagem1.src = data.image;
+        imagem1.alt=data.name;
+        nomeDoPersonagem1.innerHTML = data.name;
+        especie1.innerHTML = data.species;
+        condicao1.innerHTML= data.status;
+    });
+
 
 }
 
-botao.onclick = pegarPersonagem;
+pegarPersonagem2 = () => {
+    let numeroAleatorioPersonagem2 = gerarValorAleatorio()
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorioPersonagem2}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem2.src = data.image;
+        imagem2.alt=data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        condicao2.innerHTML= data.status;
+    });
+
+
+}
+
+pegarPersonagem3 = () => {
+    let numeroAleatorioPersonagem3 = gerarValorAleatorio()
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorioPersonagem3}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem3.src = data.image;
+        imagem3.alt=data.name;
+        nomeDoPersonagem3.innerHTML = data.name;
+        especie3.innerHTML = data.species;
+        condicao3.innerHTML= data.status;
+    });
+
+
+}
+
+function osPersonagensCapturados() {
+    pegarPersonagem2();
+    pegarPersonagem1();
+    pegarPersonagem3();
+}
+
+botao.onclick = osPersonagensCapturados
